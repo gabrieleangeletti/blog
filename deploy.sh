e4e1f97308c8b37fa15417893e8949689918246c
Gabriele Angeletti <gangeletti@lyft.com>
2019-12-20 13:19:54 +0000

    first commit

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Exit on error. Append "|| true" if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch the error in case mysqldump fails (but gzip succeeds) in `mysqldump | gzip`
+set -o pipefail
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+hugo
+
+cd public
+git add .
+COMMIT_MESSAGE="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	COMMIT_MESSAGE="$*"
+fi
+git commit -m "${COMMIT_MESSAGE}"
+git push origin master
